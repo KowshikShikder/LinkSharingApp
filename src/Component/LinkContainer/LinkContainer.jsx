32da00eba949cf6376f9e36c8fc7ecd0aa25f60d
KowshikShikder <81868349+KowshikShikder@users.noreply.github.com>
2024-10-14 03:37:39 +0600

    Update

--- src/Component/LinkContainer/LinkContainer.jsx
+++ src/Component/LinkContainer/LinkContainer.jsx
@@ -7,18 +7,14 @@ import React, { useState } from 'react'
 
 function LinkContainer(props) {
 
+
+    // Props From Parent 
     const {LinkID, LinksData, setLinksData, setLinksID, LinksID, id, index} = props;
 
 
 
-
-
-
-
-
-
-
-
+    // Platform Name, icon, color and validation
+    // Platform Name, icon, color and validation
     const [LinkInfo, setLinkInfo] = useState([
         {
             type:"Github",
@@ -53,19 +49,21 @@ function LinkContainer(props) {
 
 
 
-
+    // Link Handler
+    // Link Handler
     const LinkHandler =(uid, Names, info, typeInfo)=>{
 
 
+        // if already link stored
         if(LinksData.length > 0){
+
+            // get all ids of stored links
+
             const StoredLinksID = LinksData.map(m=> m.id)
 
-            console.log("StoredLinksID")
-            console.log(StoredLinksID)
 
+            // Check if Current ID already stored
             if(StoredLinksID.includes(uid)){
-                
-
                 let restData = LinksData.filter(f=> f.id != uid)
                 let particularData = LinksData.filter(f=> f.id == uid)
                 
@@ -73,53 +71,42 @@ function LinkContainer(props) {
                 if(typeInfo){   thisData = {...particularData[0],[Names]:info, typeInfo}}
                 else{   thisData = {...particularData[0],[Names]:info}}
                 
-
+                // Update Link
                 setLinksData([...restData, thisData])
-
-                console.log("I am here in 2nd if")
             }
             else{
+
+                // if Current ID is new  store it
                 let thisData = {name:info, id: uid, typeInfo}
                 setLinksData([...LinksData, thisData])
-
-                console.log("I am here in 2nd else")
-
             }
         }
         else{
+            // if no Link Created
             let thisData = {name:info, id: uid, typeInfo}
             setLinksData([...LinksData, thisData])
-            console.log("I am here in 1st else")
         }
-
-        
-
-
-        console.log(LinksData)
 
     }
 
 
 
 
-
-
-
+    // Link Validations
+    // Link Validations
     const GetAdressValidation=(LinkID)=>{
         try {
+
+            // Check Given URL
             const GivenLinkPart1 =  new URL(LinksData.filter(f=> f.id == LinkID)[0]?.address)?.origin?.split(".")[new URL(LinksData.filter(f=> f.id == LinkID)[0]?.address).origin.split(".").length-2]
             const GivenLinkPart2 =  new URL(LinksData.filter(f=> f.id == LinkID)[0]?.address)?.origin?.split(".")[new URL(LinksData.filter(f=> f.id == LinkID)[0]?.address).origin.split(".").length-1]
-            var combinedLink = `${GivenLinkPart1}.${GivenLinkPart2}`
+            var combinedLink = `${GivenLinkPart1.replace("https://","")}.${GivenLinkPart2}`
 
+            // Validate with stored formate
             const validationLink = LinksData.filter(f=> f.id == LinkID)[0]?.typeInfo?.Validation;
-            console.log('validationLink')
-            console.log(validationLink)
-            console.log('combinedLink')
-            console.log(combinedLink)
 
 
             if(combinedLink == validationLink){
-
                 return true
             }
             else{
@@ -130,7 +117,6 @@ function LinkContainer(props) {
         } catch (error) {
             console.log(error)
             return false
-            
         }
     }
 
@@ -140,34 +126,15 @@ function LinkContainer(props) {
 
 
 
-
-
  
+// Drag & Drop Functions
+// Drag & Drop Functions
 
-
-const [LinkDetails, setLinkDetails] = useState(
-    {
-        type:"",
-        address:"",
-        id: LinkID
-    }
-)
-
-
-
-
-
-
-
-
-
-
-const { attributes, listeners, setNodeRef, transform, transition } =
-useSortable({ id });
+const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id });
 
 const style = {
-transition,
-transform: CSS.Transform.toString(transform),
+    transition,
+    transform: CSS.Transform.toString(transform),
 };
 
 
@@ -178,61 +145,39 @@ transform: CSS.Transform.toString(transform),
   return (
         <div  style={style} className='w-full bg-gray-100 text-black text-sm px-3 py-4 rounded-md mt-4 self-center'>
             <div className='flex justify-between'> 
+
+                {/* Dragable Area for Drag & Drop */}
                 <div ref={setNodeRef} {...attributes} {...listeners} style={{touchAction:'none'}}>
                     <span className='text-3xl'> = </span><span> link #</span><span>{index+1}</span>
                 </div>
-                
+
+                {/* Link Remove Button */}
                 <div className='cursor-pointer py-1 px-2 text-sm' onClick={()=> {setLinksData([...LinksData.filter(f=> f.id != LinkID)]);   setLinksID([...LinksID.filter(d=> d.id !== LinkID)]); console.log(LinksID, LinkID)  }} >Remove</div>
             </div>
 
+            
+            
             <p className='pt-3'> Platform </p>
 
+            {/* Platform Selection */}
             <div className='relative'>
                 <p className='bg-white border border-stone-400 w-full px-3 py-2 rounded'  onClick={(e)=> {e.target.nextElementSibling.classList.remove("hidden");  }}> {LinksData.filter(f=> f.id == LinkID)[0]?.name ? LinksData.filter(f=> f.id == LinkID)[0]?.name : "Choose Platform" }  </p>
                 <div className='absolute z-20 hidden w-full bg-white mt-1 border border-stone-500'>
                     {LinkInfo.map((info, index)=> 
-                        <p key={index} className='cursor-pointer py-2 hover:bg-slate-600 hover:text-white border-t px-3' onClick={(e)=> { e.target.parentElement.previousSibling.innerHTML=  `<i className="fab ${info.icon} mr-1"> </i>  ${info.type}`; setLinkDetails({...LinkDetails, type: info.type, typeInfo:info}); LinkHandler(LinkID, "type", info.type, info);  e.target.parentElement.classList.add("hidden")   } } > <i className={`fab ${info.icon} mr-1`}></i>  {info.type} </p>    
+                        <p key={index} className='cursor-pointer py-2 hover:shadow hover:border hover:bg-slate-600 hover:text-white border-black bg-white  border-t px-3'  onClick={(e)=> { e.target.parentElement.previousSibling.innerHTML=  `<i className="fab ${info.icon} mr-1"> </i>  ${info.type}`; LinkHandler(LinkID, "type", info.type, info);  e.target.parentElement.classList.add("hidden")   } } > <i className={`fab ${info.icon} mr-1`}></i>  {info.type} </p>    
                     )}
                 </div>
             </div>
 
 
-
-
-
-            {/* animation: {
-                        shake:'shake 0.5s cubic-bezier(0.36, 0.07, 0.19, 0.97)'
-                              },
-                                    keyframes: {
-                                            shake : {
-                                                      '10%, 90%': {
-                                                                    transform: 'translate3d(-1px, 0, 0)'
-                                                                              },
-                                                                                        '20%, 80%': {
-                                                                                                      transform: 'translate3d(2px, 0, 0)'
-                                                                                                                },
-                                                                                                                          '30%, 50%, 70%': {
-                                                                                                                                        transform: 'translate3d(-4px, 0, 0)'
-                                                                                                                                                  },
-                                                                                                                                                            '40%, 60%': {
-                                                                                                                                                                          transform: 'translate3d(4px, 0, 0)'
-                                                                                                                                                                                    }
-            } */}
-
-
-
-
-
-
-
-
+            {/* platform Adress */}
             <p className='mt-3'> Link </p>
             <div className='relative'>
-                <input type='text' defaultValue={LinksData.filter(f=> f.id == LinkID)[0]?.address ? LinksData.filter(f=> f.id == LinkID)[0]?.address : ""} name='address' className='border border-stone-400 w-full px-3 py-2 rounded pl-8' onChange={test=> {setLinkDetails({...LinkDetails, address: test.target.value}); console.log(LinkDetails); console.log(LinkID); LinkHandler(LinkID, test.target.name, test.target.value);   } } />
+                <input type='text' defaultValue={LinksData.filter(f=> f.id == LinkID)[0]?.address ? LinksData.filter(f=> f.id == LinkID)[0]?.address : ""} name='address' className='border border-stone-400 w-full px-3 py-2 rounded pl-8' onChange={test=> { console.log(LinkID); LinkHandler(LinkID, test.target.name, test.target.value);    }} />
                 <i className="fas fa-link absolute left-3 top-3"></i>
                 
-                {LinksData.filter(f=> f.id == LinkID)[0]?.typeInfo && LinksData.filter(f=> f.id == LinkID)[0]?.address?.length > 5 && !GetAdressValidation(LinkID) &&
-                 <div className='bg-red-600 px-2 text-center rounded mt-1 py-2 text-white w-40 text-xs'> Link of <span className='font-bold uppercase italic'>{LinksData.filter(f=> f.id == LinkID)[0]?.typeInfo?.type}</span>  is not appropiete. </div>
+                {LinksData.filter(f=> f.id == LinkID)[0]?.typeInfo && !GetAdressValidation(LinkID) &&
+                 <div className='bg-red-600 px-2 text-center rounded mt-1 py-2 text-white w-56 text-xs'> Link of <span className='font-bold uppercase italic'> {LinksData.filter(f=> f.id == LinkID)[0]?.typeInfo?.type} </span>  is not appropiete. </div>
                 }
              </div>
         </div>
---
+++ src/app/Profile/[ProfileID]/page.jsx
@@ -0,0 +1,260 @@
+'use client'
+
+import { db } from "@/Lib/firebase";
+import { doc, getDoc } from "firebase/firestore";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+
+
+
+
+
+export default function Page({ params }) {
+
+// Get Id from Params
+    const UserID = params.ProfileID;
+    
+    
+    
+    // IF id Found Search it
+    useEffect(() => {
+            if(UserID){
+                UpdateUser()
+            }
+        }, [UserID])
+
+
+
+
+
+
+
+
+
+
+
+    // user info state
+    // user info state
+
+    const [UserInfo, setUserInfo] = useState(
+        {
+            FirstName:"",
+            LastName:"",
+            Email:"",
+            Image:{
+                file: null,
+                url: null,
+            },
+            Password:"",
+            userID:""
+        }
+    )
+
+
+// links Data stats
+// links Data stats
+    const [LinksData, setLinksData] = useState([])
+    const [LinksID, setLinksID] = useState([])
+    const [Loading, setLoading] = useState("Loading")
+
+
+
+
+
+
+
+
+
+
+
+
+    // Fetch Data From Database
+    // Fetch Data From Database
+    const UpdateUser = async()=>{
+    
+
+        // setloading
+        setLoading("Loading")
+    
+
+        // Fetch Data
+        if (UserID) {
+            try {
+            const docRef = doc(db, "users", UserID);
+            const docSnap = await getDoc(docRef);
+    
+            if (docSnap.exists()) {
+    
+                let FetchedData = docSnap.data();
+    
+
+                // Store Data in local States
+                setUserInfo( 
+                                {
+                                    FirstName:  FetchedData.firstName,
+                                    LastName:   FetchedData.lastName,
+                                    Email:  FetchedData.email,
+                                    userID:  FetchedData.userId,
+                                    Image:{
+                                        file: null,
+                                        url: FetchedData.avatar,
+                                    }
+                                }
+                            )
+    
+                setLinksID(FetchedData.LinksIdContainer);
+                setLinksData(FetchedData.LinksContainer);
+    
+
+                // if found set state found
+                if(FetchedData.LinksIdContainer.length > 0){
+                    setLoading("Found")
+                }
+            
+                return  console.log(docSnap.data())
+            } else {
+                // if data not found set state not found
+                setLoading("Not Found")
+                return console.log("No User data")
+            }
+            } catch (err) {
+            setLoading("Not Found")
+            console.log(err);
+            return  console.log("No User")
+            }
+    
+    
+    
+        } else {
+                setLoading("Not Found")
+                console.log('No user is signed in.');
+        }
+    }
+
+    
+    
+    
+    
+    
+    
+    // Copy to Clipboard
+    // Copy to Clipboard
+    const copyToClipboard =(e)=>{
+
+        if(UserInfo.userID){
+
+            var copyText = `${window.location.origin}/Profile/${UserInfo.userID}`
+            copy(copyText);
+            e.target.innerHTML = `Copid  <i class="ml-2 far fa-clipboard"></i>`;
+
+
+            setTimeout(() => {
+                e.target.innerHTML = `Share link`;
+            }, 2000);
+
+        }
+        else{
+            alert("Please Save your information!")
+        }
+
+    }
+
+
+    
+    
+    
+
+
+
+
+return (
+    <>
+    
+    {UserInfo && 
+
+
+
+    <div className='flex flex-col bg-white justify-center items-center px-6'>
+
+        <div className='bg-blue-600 h-52 w-full overflow-hidden absolute top-0 rounded-b-3xl'></div>
+
+
+
+
+        <div className='p-2 px-6 w-full shadow z-20 bg-white flex h-14 items-center justify-between mt-4 mx-8 rounded-md'> 
+            
+            
+            <div className="left-section flex">
+                <img src="/Kahf Logo.jpg" alt="Lgog" className='h-6 w-6 mr-2' />
+                <p className='hidden md:block text-black font-extrabold'> devlinks </p>
+            </div>
+
+            <div className='text-xs md:text-sm border hover:border-blue-700 hover:text-blue-700 hover:bg-white border-black  bg-blue-700 text-white rounded p-1 px-3 md:px-6 cursor-pointer hover:scale-105 duration-300' onClick={(e)=> copyToClipboard(e)}> Share link </div>
+        </div>
+
+
+
+
+
+
+
+        
+
+        {/* Mobile Holder Section */}
+        {/* Mobile Holder Section */}
+
+        <div className='bg-white shadow-md border text-black border-black z-20 my-20 rounded-3xl w-60 px-8 py-8 flex flex-col items-center' style={{minHeight:"420px"}}>  
+            <img src={UserInfo.Image.url ? UserInfo.Image.url : '/avatar.png'} alt="" className='h-16 w-16 rounded-full border-purple-800 border-2' />
+            <p className='text-sm font-bold mt-3'>  {UserInfo.FirstName} {UserInfo.LastName} </p>
+            <p className='text-xs mt-3 mb-4'> {UserInfo.Email} </p>
+
+
+
+
+
+
+            {
+            Loading == "Loading" ? 
+                <div className="h-36 w-full mt-6">
+                    <img src="/Loading.gif" alt="" className="h-full w-full object-cover" />
+                    <p className="text-center font-bold mt-4">Loading...</p>
+                </div>
+
+            :
+
+            Loading == "Found" ?
+                LinksData.length > 0 && LinksData.map(x=>
+                    <Link key={x.id} href={x.address ? x.address : ""} target="_blank" className='w-full'> 
+                        <div className={`flex text-white text-xs font-extralight mt-3 h-10 justify-between items-center ${x.typeInfo?.color} px-4 rounded-md w-full hover:scale-105 duration-300 cursor-pointer hover:shadow-md`}>
+                            <div>   <i className={`fab ${x.typeInfo?.icon} mr-1`}></i>  <span> {x.typeInfo?.type} </span>   </div>
+                            <i className="fas fa-arrow-right"></i>
+                        </div>
+                    </Link>
+                )
+
+            :
+                <div className="h-36 w-full mt-6">
+                    <img src="/NotFound.gif" alt="" className="h-full w-full object-cover" />
+                    <p className="text-center font-bold mt-4">Not Found</p>
+                </div>
+            }
+
+
+
+
+
+        </div>
+    </div>
+
+
+    }
+</>
+)
+}
+
+
+
+
+
+
